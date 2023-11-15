22bbaeea4e140f82172f4c7f3e1de14564ed4cce
Francisco_Diaz_Roldan <pacodiaz008@gmail.com>
2023-11-15 19:01:22 +0100

    Clase Producto completada.

--- src/main/java/com/example/gestordepedidoshibernate/domain/producto/Producto.java
+++ src/main/java/com/example/gestordepedidoshibernate/domain/producto/Producto.java
@@ -1,73 +1,30 @@
 package com.example.gestordepedidoshibernate.domain.producto;
-/**
- * Clase que representa un producto en el sistema de gestión de pedidos.
- */
-public class Producto {
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Entity@Table(name = "producto")
+public class Producto implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_producto;
+    @Column(name="nombre")
     private String nombre;
+    @Column(name = "precio")
     private Double precio;
+    @Column(name = "cantidad_disponible")
     private Integer cantidad_disponible;
 
-    /**
-     * Constructor que crea un nuevo objeto Producto con los atributos especificados.
-     *
-     * @param id_producto         Hace referencia al ID del producto.
-     * @param nombre              Se refiere al nombre del producto.
-     * @param precio              Se refiere al precio del producto.
-     * @param cantidad_disponible Hace referencia a la cantidad disponible del producto en el inventario.
-     */
-    public Producto(Integer id_producto, String nombre, Double precio, Integer cantidad_disponible) {
-        this.id_producto = id_producto;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.cantidad_disponible = cantidad_disponible;
-    }
-
-    /**
-     * Constructor sin argumentos que crea un objeto Producto vacío.
-     */
-    public Producto() {
-    }
-
-    public Integer getId_producto() {
-        return id_producto;
-    }
-
-    public void setId_producto(Integer id_producto) {
-        this.id_producto = id_producto;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(Double precio) {
-        this.precio = precio;
-    }
-
-    public Integer getCantidad_disponible() {
-        return cantidad_disponible;
-    }
-
-    public void setCantidad_disponible(Integer cantidad_disponible) {
-        this.cantidad_disponible = cantidad_disponible;
-    }
-
-    /**
-     * Sobrescribe el método toString para representar el producto como una cadena de texto.
-     *
-     * @return Una representación de cadena del producto en el formato "nombre precio€".
-     */
     @Override
     public String toString() {
-        return nombre + " " + precio + "€";
+        return "Producto{" +
+                "id_producto=" + id_producto +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", cantidad_disponible=" + cantidad_disponible +
+                '}';
     }
 }
